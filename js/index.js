2edaaa1fb7e8be7419d2915261006aedcee6ccd7
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2020-10-14 23:31:04 +0530

    linter error corrected

--- js/index.js
+++ js/index.js
@@ -1,136 +1,125 @@
+const find = (sel) => document.querySelector(sel);
+const cells = document.querySelectorAll('.cell');
+const startGameButton = find('.startGame');  // eslint-disable-line
+
+
 const gameBoard = () => {
-	let board = {
-		1: '',
-		2: '',
-		3: '',
-		4: '',
-		5: '',
-		6: '',
-		7: '',
-		8: '',
-		9: '',
-	};
+  let board = {
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: '',
+  };
 
-	const winCase = [
-		[1, 2, 3],
-		[4, 5, 6],
-		[7, 8, 9],
-		[1, 4, 7],
-		[2, 5, 8],
-		[3, 6, 9],
-		[1, 5, 9],
-		[3, 5, 7],
-	];
+  const winCase = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
 
-	const checkWin = () => {
-		return winCase.some((ar) => {
-			return (
-				board[ar[0]] === board[ar[1]] &&
-				board[ar[1]] === board[ar[2]] &&
-				board[ar[2]] !== ''
-			);
-		});
-	};
+  const checkWin = () => winCase.some((ar) => (
+    board[ar[0]] === board[ar[1]] && board[ar[1]] === board[ar[2]] && board[ar[2]] !== ''
+  ));
 
-	const attacheLiseter = (func) => {
-		cells.forEach((el) => {
-			el.onclick = func;
-		});
-	};
+  const attacheLiseter = (func) => {
+    cells.forEach((el) => {
+      el.onclick = func;
+    });
+  };
 
-	const setBoard = (newState) => (board = { ...newState });
-	const getCellValue = (i) => Object.values(board)[i];
+  const setBoard = (newState) => {
+    board = { ...newState };
+    return board;
+  };
+  const getCellValue = (i) => Object.values(board)[i];
 
-	const boardCellUpdate = (index, sign) => {
-		board[index] = sign;
-		render();
-	};
+  const render = () => {
+    cells.forEach((el, i) => {
+      el.innerText = getCellValue(i);
+    });
+  };
 
-	const render = () => {
-		cells.forEach((el, i) => {
-			el.innerText = getCellValue(i);
-		});
-	};
+  const boardCellUpdate = (index, sign) => {
+    board[index] = sign;
+    render();
+  };
 
-	return {
-		setBoard,
-		getCellValue,
-		checkWin,
-		render,
-		attacheLiseter,
-		boardCellUpdate,
-	};
+
+  return {
+    setBoard,
+    getCellValue,
+    checkWin,
+    render,
+    attacheLiseter,
+    boardCellUpdate,
+  };
 };
 
-const player = (name, sign) => {
-	return { name, sign };
-};
+const player = (name, sign) => ({ name, sign });
 
 const game = (pl1, pl2) => {
-	let play1 = pl1;
-	let play2 = pl2;
-	let sign = '';
-	let id = 0;
-	let countMove = 1;
-	let board = gameBoard();
+  let play1 = pl1;
+  let play2 = pl2;
+  let sign = '';
+  let id = 0;
+  let countMove = 1;
+  const board = gameBoard();
+  const findWinner = () => (sign === play1.sign ? play1.name : play2.name);
 
-	const onMove = (e) => {
-		id = parseInt(e.currentTarget.id);
+  const onMove = (e) => {
+    id = parseInt(e.currentTarget.id); // eslint-disable-line
 
-		if (board.getCellValue(id - 1) === '') {
-			sign = countMove % 2 === 0 ? 'O' : 'X';
-			countMove += 1;
-			board.boardCellUpdate(id, sign);
+    if (board.getCellValue(id - 1) === '') {
+      sign = countMove % 2 === 0 ? 'O' : 'X';
+      countMove += 1;
+      board.boardCellUpdate(id, sign);
+    }
+    if (board.checkWin() || countMove >= 10) {
+      if (countMove >= 10) {
+        alert('Game Over'); // eslint-disable-line
+      } else {
+        const word = `You win ${findWinner()}`;
+        alert(word);  // eslint-disable-line
+      }
+      location.reload();  // eslint-disable-line
+    }
+  };
 
-			console.log(sign);
-		}
-		if (board.checkWin() || countMove >= 10) {
-			if (countMove >= 10) {
-				alert('Game Over');
-			} else {
-				const word = `You win ${findWinner()}`;
-				alert(word);
-			}
-			location.reload();
-		}
-	};
 
-	const findWinner = () => {
-		return sign === play1.sign ? play1.name : play2.name;
-	};
+  const initGame = () => {
+    const pl1 = find('.pl1');
+    const pl2 = find('.pl2');
+    if (pl1.value && pl2.value) {
+      play1 = player(pl1.value, 'X');
+      play2 = player(pl2.value, 'O');
+      find('.welcome').classList.add('d-none');
+      find('.board').classList.remove('d-none');
+      pl1.value = '';
+      pl2.value = '';
+      board.attacheLiseter(onMove);
+    } else {
+      alert('Please Enter Player Name'); // eslint-disable-line
+    }
+  };
 
-	const initGame = () => {
-		let pl1 = find('.pl1');
-		let pl2 = find('.pl2');
-		if (pl1.value && pl2.value) {
-			play1 = player(pl1.value, 'X');
-			play2 = player(pl2.value, 'O');
-			find('.welcome').classList.add('d-none');
-			find('.board').classList.remove('d-none');
-			pl1.value = '';
-			pl2.value = '';
-			board.attacheLiseter(onMove);
-		} else {
-			alert('Please Enter Player Name');
-		}
-	};
-
-	startGameButton.onclick = (e) => {
-		e.preventDefault();
-		initGame();
-	};
-	return {};
+  startGameButton.onclick = (e) => {  // eslint-disable-line
+    e.preventDefault();
+    initGame();
+  };
+  return {};
 };
 
-const find = (sel) => {
-	return document.querySelector(sel);
-};
-
-// Selectors and Variables
-const cells = document.querySelectorAll('.cell');
-const startGameButton = find('.startGame');
-let players = [];
 
 window.onload = () => {
-	const newGame = game();
+  game();
 };
